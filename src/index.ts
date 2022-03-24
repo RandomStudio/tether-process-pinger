5d132a11a4fdb27cd51b29789e83853c602e2064
Stephen Buchanan <stephen.buchanan@random.studio>
2022-03-24 16:53:55 +0100

    init commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import defaults from "./defaults";
+import parse from "parse-strings-in-object";
+import rc from "rc";
+import { getLogger } from "log4js";
+
+const appName = "TetherProcessPinger";
+
+const config: typeof defaults = parse(rc(appName, defaults));
+
+const logger = getLogger(appName);
+logger.level = config.loglevel;
+
+logger.info("started with config", config);
